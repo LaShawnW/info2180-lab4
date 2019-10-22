748c61216d594c81bab5dcebd7bd1129cc151d31
La Shawn Williams <lashawnwilliams98@outlook.com>
2019-10-21 23:43:25 -0500

    lab4

--- info2180-lab4-tic-tac-toe-master/info2180-lab4-tic-tac-toe-master/tic-tac-toe.js
+++ info2180-lab4-tic-tac-toe-master/info2180-lab4-tic-tac-toe-master/tic-tac-toe.js
@@ -1,7 +1,12 @@
 "use strict";
 
+let chance=0;
+let setup;
+let gameOver= false; //will be changed to true if there is a winner
 let gameState= []; //keeps track of what has been played
 let plays=["X","O"];//options person has to play
+
+let status= null;
 
 
 window.onload= function() {
@@ -10,29 +15,42 @@ window.onload= function() {
 
     
     var game = document.getElementById("board").querySelectorAll("div");
+    setup= game.children;
 
     for(let square of game){
         square.className='square';// add class to square
         square.onclick= () => {clickSquare(square)};
         square.onmouseover= () =>{squareHover(square)};
         square.onmouseout= () =>{noHover(square)};
-    }          
+    }    
+    
+    let restartGame= document.getElementsByClassName("btn");
+    restartGame[0].addEventListener("click",()=>{this.location.reload();});
 }
 
 
 function clickSquare(square){
 //want to place either a X or a O into the square 
     var val= gameState[gameState.length-1];
-    if (gameState==[]){
-        playX(square);
-    }//checking if array is empty before action         
-    else if(val==="X"){
-        play0(square);//next click will result in a O
-    }
-    else{
-        playX(square);//next click will result in a X
-    }    
-        
+    //if(!this.innerText && !gameOver){
+    
+        if (gameState==[]){
+            playX(square);
+            chance++;
+        }//checking if array is empty before action         
+        else if(val==="X"){
+            play0(square);//next click will result in a O
+            chance++;
+        }
+        else{
+            playX(square);//next click will result in a X
+            chance++
+        }
+        if(chance>3){
+            isWinner();
+        }
+    //}
+    
 }
 
 function playX(square){
@@ -58,3 +76,44 @@ function squareHover(square){
 function noHover(square){
     square.classList.remove("hover");
 }
+
+//checks for a winner of the game
+function isWinner(){
+    const check= (s1,s2,s3) =>{
+        if((setup[s1].innerHTML===setup[s2].innerHTML)&& (setup[s1].innerHTML===setup[s3].innerHTML)&& (setup[s2].innerHTML===setup[s3].innerHTML)){
+            return true;
+        }
+        else{
+            return false;
+        }
+
+    }
+    //check for wins in vertical
+    wonGame=check(0,3,6) || check(1,4,7) || check(2,5,8);
+    
+    //check for wins in horizontal
+    wonGame= wonGame || check(0,1,2) || check(3,4,5) || check(6,7,8);
+
+    //check for diagonal wins
+    wonGame= wonGame || check(0,4,8) || check(2,4,6);
+
+    if(wonGame){
+        const msg= document.getElementById("status");
+        youWon();
+        gameWon=true;
+    }
+    else{
+        if(chance==8){
+            status.textContent= "DRAW"
+        }
+    }
+      
+}
+
+function youWon(msg){
+    status.textContent= "Congratulations!" + gameState[gameState.length-1]+ "is the Winner!";
+    status.className= "you-won";
+    console.log("PERSON WON");
+}
+
+
